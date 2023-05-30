379ce47ad8076071ca405ff7a10be598a2e0751f
Abhiram Pai <abhirampai1999@gmail.com>
2023-05-30 09:51:41 +0530

    Added conversion container

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import ConverterContainer from "./ConverterContainer";
+import Header from "./Header";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <Header />
+      <ConverterContainer />
+    </div>
+  );
+};
+
+export default Main;
